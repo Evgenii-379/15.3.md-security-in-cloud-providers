5f36b0555ed19af23226c308d1cc02149a0ffe10
Evgenii-379 <152536714+Evgenii-379@users.noreply.github.com>
2025-04-26 23:05:20 +0300

    Create ssh_keys.tf

---
+++ config/ssh_keys.tf
@@ -0,0 +1,4 @@
+variable "public_ssh_key" {
+  description = "Публичный SSH-ключ для доступа к ВМ"
+  default     = "ssh-ed25519 AAAAC3Nz...................................V5QM evgeniy_0307@mail.ru"
+}
